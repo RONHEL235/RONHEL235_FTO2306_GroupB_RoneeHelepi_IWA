bf6d5bb0b380879253b906a96815ed93575143ae
Ronee <mataupana10@gmail.com>
2023-08-01 10:58:54 +0200

    Fixed scripts.js file so that the app can properly add and subtract numbers

--- IWA1/scripts.js
+++ IWA1/scripts.js
@@ -6,11 +6,12 @@ const subtract = document.querySelector('[data-key="subtract"]')
 const add = document.querySelector('[data-key="add"]')
 
 const subtracthandler = () => {
+
     const newValue = parseInt(number.value) - 1
     number.value = newValue
 
     if (add.disable === true) {
-        add.disabled = true
+        add.disabled = false
     }
 
     if (newValue <= MIN_NUMBER) {
@@ -23,13 +24,17 @@ const addhandler = () => {
     number.value = newValue
 
     if (subtract.disable === true) {
-        subtract.disabled = true
+        subtract.disabled = false
     }
 
-    if (newValue <= MIN_NUMBER) {
-        subtract.disabled = true
+    if (newValue >= MAX_NUMBER) {
+        add.disabled = true
     }
 }
+
+subtract.addEventListener('click', subtracthandler)
+add.addEventListener('click', addhandler)
+
 
 
 
