a9ab2be0713573b1d65d55bc1b99a3529308a602
Ronee <mataupana10@gmail.com>
2023-08-01 14:51:52 +0200

    Fixed all error syntax that was making the code not run

---
+++ IWA2/challange3.js
@@ -0,0 +1,14 @@
+/** 
+It is important to show the following message in order to:
+- to assure users
+- to scare hackers
+- to impress investors */
+
+const warning = "Security scan starting"
+console.warn(warning)
+
+
+/** It is important to let user know when they can close the page */
+
+const information = "Please wait for scan to complete before closing the browser."
+console.info(information)
---
+++ IWA2/challenge1.js
@@ -0,0 +1,15 @@
+/* FreeCodeCamp username 
+* fcca779ce4f-0624-434d-9641-da7afc76ba2a
+*/
+
+
+/* This is the firstname of the user */
+
+const user = 'John'
+
+
+/* This is the lastname of the user */ 
+const surname = 'Smith'
+
+
+console.log(user, surname)
