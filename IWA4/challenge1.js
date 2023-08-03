dc29f40392c4f7ae6972deb7c1eb87ca3545ace7
Ronee <mataupana10@gmail.com>
2023-08-03 17:17:17 +0200

    Used the let declaration on all variables on global scope so that they can be modified in the conditional statements

--- IWA4/challenge1.js
+++ IWA4/challenge1.js
@@ -1,18 +1,18 @@
-const date = 2050
-const status1 = 'student'
-const count = 0
+let date = 2050
+let role = "student"
+let count = 0
 
-if (date == 2050) {
-	console.log('January', 'New Year’s Day')
-	console.log('March', 'Human Rights Day')
+if (date = 2050) {
+	console.log("January", 'New Year’s Day')
+	console.log("March", 'Human Rights Day')
 	date = 'April'
 	console.log(date, 'Family Day')
 	console.log(date, 'Freedom Day')
 	count = count + 4
 
-	if (status1 = 'student') {
-	    console.log('June', 'Youth Day')
-	    count = count + 1
+	if (role = "student") {
+	  	console.log('June', 'Youth Day')
+		count = count + 1
   }
 
 	console.log('August', 'Women’s Day')
@@ -21,15 +21,15 @@ if (date == 2050) {
 	console.log(date, 'Day of Reconciliation')
 	count = count + 3
 
-	if (status1 = "parent") {
-	    console.log(date, 'Christmas Day')
-	    count = count + 1
+	if (role = "parent") {
+	  	console.log(date, 'Christmas Day')
+		count = count + 1
   }
 
 	console.log(date, 'Day of Goodwill')
 	count = count + 1
 }
 
-console.log('Your status is:', status1)
+console.log('Your status is:', role)
 console.log('The year is:', date)
 console.log('The total holidays is:', count)
---
+++ IWA4/practice.js
@@ -0,0 +1,9 @@
+      
+const value = 18
+if (true) {
+    
+    console.log(value)
+    if (true) {
+        console.log(value)
+    }  
+}
