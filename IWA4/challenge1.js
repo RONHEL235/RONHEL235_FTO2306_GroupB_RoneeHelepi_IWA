9b251a8815357f0c96ee91939179ebd80930a8f7
Ronee <mataupana10@gmail.com>
2023-08-05 14:35:49 +0200

    Changed all the content of the variables to have only single quotes for consistancy

--- IWA4/challenge1.js
+++ IWA4/challenge1.js
@@ -1,16 +1,16 @@
 const date = 2050
-const role = "student"
+const role = 'student'
 let count = 0
 
 if (date == 2050) {
-	console.log("January", 'New Year’s Day')
-	console.log("March", 'Human Rights Day')
+	console.log('January', 'New Year’s Day')
+	console.log('March', 'Human Rights Day')
 	let date = 'April'
 	console.log(date, 'Family Day')
 	console.log(date, 'Freedom Day')
 	count = count + 4
 
-	if (role == "student") {
+	if (role == 'student') {
 		console.log('June', 'Youth Day')
 		count = count + 1
 	}else{}
@@ -21,7 +21,7 @@ if (date == 2050) {
 	console.log(date, 'Day of Reconciliation')
 	count = count + 3
 
-	if (role == "parent") {
+	if (role == 'parent') {
 		console.log(date, 'Christmas Day')
 		count = count + 1
 	}else{}
