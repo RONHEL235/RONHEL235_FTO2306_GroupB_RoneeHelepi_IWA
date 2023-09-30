6de31154f39dbc22375e8339b506eca15e4d4ade
Ronee <mataupana10@gmail.com>
2023-09-30 10:20:14 +0200

    Added a mark down file to IWA17

---
+++ IWA17/markdow.md
@@ -0,0 +1,33 @@
+# Calendar Website
+
+This calendar website is designed to be simple and user friendly, with the aim of providing just a minimal functionality of highlighting the current date so that the user ca identify what day it is. The website provides all the basic features you would expect from a calendar, like the month and year as the header of the calendar, rows indicating weeks, columns indicating the days, the inner blocks indicating the date number of the month and the current date being highlighted.
+
+## Features
+
+- Simple and minimal look
+- Ability to know the current date
+
+## Requirements
+
+- An IDE like [Visual Studio Code](https://code.visualstudio.com/). 
+- Basic [HTML, CSS and Javascript](https://developer.mozilla.org/en-US/docs/Learn). 
+- A browser like [Brave](https://brave.com/).
+
+## Getting Started 
+
+Clone the repository
+```
+git clone https://github.com/RONHEL235/RONHEL235_FTO2306_GroupB_RoneeHelepi_IWA
+
+```
+
+Run localHost server
+
+Open `index.html`
+
+## User stories 
+
+-  As a user, I want to see the current month and year as a heading of the calender so that I can see the current month and year we are in.
+- As a user, I want to see the number of weeks in row form so that I can identify what week we are currently in.
+- As a user, I want to see day of the weeks in column form so that I can identify what day of the week it is.
+- As a user I want the current day to be highlighted so that I can identify the current day of the month.        
--- IWA17/scripts.js
+++ IWA17/scripts.js
@@ -1,3 +1,5 @@
+// @ts-check 
+
 const MONTHS = [
     'January',
     'February',
@@ -16,7 +18,15 @@ const MONTHS = [
 const getDaysInMonth = (date) => new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 
 // Only edit below
-/* 1 */
+/**
+ * The function `createArray` generates an array of integers where each element value is equal to its index in the array.
+ * It initializes an empty array named `result`.
+ * It then iterates from 0 up to, but not including, the specified `length`, incrementing the index `i` by 1 in each iteration.
+ * Inside the loop, it adds the current value of `i` to the `result` array using the `push` method.
+ * Finally, it returns the populated `result` array.
+ * @param {number} length - The desired length of the output array.
+ * @returns {number[]} result - An array of integers from 0 up to `length` - 1.
+ */
 const createArray = (length) => {
     const result = []
 
@@ -27,8 +37,18 @@ const createArray = (length) => {
     // return a list that takes in the argument length, does some looping and gives it back
 }
 
-/* 2 */
-
+/**
+ * The function `createData` constructs a structured data representation of a calendar month, 
+ * with an array of weeks, where each week contains an array of days.
+ * It initializes a Date object `current` to the current date, then sets the date to the 1st of the current month.
+ * It obtains the starting day of the week for the 1st of the month (`startDay`), and the total number of days in the month (`daysInMonth`).
+ * It creates arrays `weeks` (with 5 elements) and `days` (with 7 elements) representing the weeks in a month and the days in a week, respectively.
+ * It iterates through each week, creating an object for each week containing a `week` number and an empty `days` array.
+ * Within each week, it iterates through each day, calculating the day of the month (`day`) and checking if it's a valid day in the current month (`isValid`).
+ * For each day, it populates the `days` array of the current week object with a `dayOfWeek` and `value` (either the day of the month or an empty string if not valid).
+ * Finally, it returns the structured `result` array containing the calendar data for the current month.
+ * @returns {Object[]} result - An array of week objects, each containing a week number and an array of day objects for the current month.
+ */
 const createData = () => {
     const current = new Date();
     current.setDate(1);
@@ -59,7 +79,18 @@ const createData = () => {
     return result;
 };
 
-/* 3 */
+/**
+ * The function `addCell` appends an HTML table cell (`<td>`) to the provided existing HTML string.
+ * 1. It takes three arguments: the existing HTML string, a class string to be added to the new cell, and a value to be inserted into the cell.
+ * 2. Using a template literal, it appends a new `<td>` element with the provided class string and value to the existing HTML string.
+ * 3. The value within the cell is surrounded by non-breaking spaces (`&nbsp;`) for spacing purposes.
+ * 4. The resulting concatenated HTML string is then returned.
+ * @param {string} existing - The existing HTML string.
+ * @param {string} classString - The CSS class to be applied to the new cell.
+ * @typedef {string|number} Value - The value to be inserted into the new cell.  
+ * @param {Value} value 
+ * @returns {string} result - The updated HTML string with the appended table cell.
+ */
 const addCell = (existing, classString, value) => {
     const result = /* html */ `
         ${existing}
@@ -71,7 +102,19 @@ const addCell = (existing, classString, value) => {
     return result;
 }
 
-/* 4 */
+/**
+ * The function `createHtml` constructs an HTML string representing a table of weeks and days based on the provided data.
+ * It initializes an empty string `result` to store the final HTML output.
+ * Iterates through each `week` object in the provided `data` array, initializing an empty string `inner` to store the HTML for the current row.
+ * For each `week`, it first appends a cell indicating the week number using the `addCell` function.
+ * Then, it iterates through each `day` object in the `days` array of the current `week`, determining whether the day is 'today', a 'weekend', or falls in an 'alternate' week.
+ * Constructs a `classString` to store CSS class names based on the conditions evaluated, which will be applied to the current day's cell.
+ * Utilizes the `addCell` function to append a cell for the current day to the `inner` string, with the constructed `classString` and day's `value`.
+ * After processing all the days for the current week, it appends a new row (`<tr>`) to the `result` string, enclosing the `inner` string, representing all the days of the current week.
+ * Finally, after processing all the weeks, it returns the constructed `result` string, which now represents a table of weeks and days.
+ * @param {Object[]} data - An array of week objects, each containing a week number and an array of day objects.
+ * @returns {string} result - The constructed HTML string representing a table of weeks and days.
+ */
 const createHtml = (data) => {
     let result = '';
 
